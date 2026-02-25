ebd5500351ed44e4430626230fe91ab3e618b797
ShreySharma15 <shrey1atharv@gmail.com>
2026-02-25 09:14:59 +0530

    UC9 : Recursive Palindrome Checker

--- PalindromeChecker/src/PalindromeCheckerApp.java
+++ PalindromeChecker/src/PalindromeCheckerApp.java
@@ -1,43 +1,39 @@
 /*
  * MAIN CLASS - PalindromeCheckerApp
- * UC6: Queue + Stack Based Palindrome Check
+ * UC9 : Recursive Palindrome Checker
  *
  * Description:
  * This class represents the palindrome checker app
  *
  * At this stage, the application:
- * - Validates a palindrome using a Deque by
- * - comparing front and rear characters directly.
+ * - Validates a palindrome using recursion
+ * - comparing start and end characters
  * - Displays formatted result on console
  *
  *
  * @author Shrey Sharma
- * @version 7.0
+ * @version 9.0
  */
 
-import java.util.ArrayDeque;
-import java.util.Deque;
 
 public class PalindromeCheckerApp {
     public static void main(String[] args) {
-        String input = "refer";
-        Deque<Character> deque = new ArrayDeque<>();
+        String input = "madam";
 
-        for (char c : input.toCharArray()) {
-            deque.add(c);
-        }
+        boolean result = check(input, 0, input.length() - 1);
 
-        boolean isPalindrome = true;
-
-        while (deque.size() > 1) {
-
-            if (deque.removeFirst() != deque.removeLast()) {
-                isPalindrome = false;
-                break;
-            }
-        }
-        
         System.out.println("Input : " + input);
-        System.out.println("Is Palindrome? : " + isPalindrome);
+        System.out.println("Is Palindrome? : " + result);
+    }
+
+    private static boolean check(String s, int start, int end){
+        if (start >= end) {
+            return true;
+        }
+
+        if (s.charAt(start) != s.charAt(end)) {
+            return false;
+        }
+        return check(s, start + 1, end - 1);
     }
 }
