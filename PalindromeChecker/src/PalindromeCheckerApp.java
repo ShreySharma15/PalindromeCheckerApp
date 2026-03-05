c6628349255809fafe298478d83eb493e60b52a0
ShreySharma15 <shrey1atharv@gmail.com>
2026-03-05 09:04:02 +0530

    UC13: Performance Comparison

--- PalindromeChecker/src/PalindromeCheckerApp.java
+++ PalindromeChecker/src/PalindromeCheckerApp.java
@@ -1,39 +1,50 @@
 /*
  * MAIN CLASS - PalindromeCheckerApp
- * UC9 : Recursive Palindrome Checker
+ * UC13: Performance Comparison
  *
  * Description:
  * This class represents the palindrome checker app
  *
  * At this stage, the application:
- * - Validates a palindrome using recursion
- * - comparing start and end characters
- * - Displays formatted result on console
+ * - Validates a palindrome using reversing a string
+ * - Captures execution start and end time and calculates total execution duration
+ * - Calculate and displays formatted result on console
  *
  *
  * @author Shrey Sharma
- * @version 9.0
+ * @version 10.0
  */
 
 
 public class PalindromeCheckerApp {
     public static void main(String[] args) {
-        String input = "madam";
-
-        boolean result = check(input, 0, input.length() - 1);
+        String input = "level";
+        long start = System.nanoTime();
+        boolean reversePalindrome = reverseIsPalindrome(input);
+        long end = System.nanoTime();
+        long duration = end - start;
 
         System.out.println("Input : " + input);
-        System.out.println("Is Palindrome? : " + result);
+        System.out.println("Is Palindrome? : " + reversePalindrome);
+        System.out.println("Execution time: " + duration + " ns");
     }
-
-    private static boolean check(String s, int start, int end){
-        if (start >= end) {
-            return true;
+    private static boolean reverseIsPalindrome(String input){
+        char[] reversed = new char[input.length()];
+        boolean isPalindrome = false;
+        for(int i = input.length() - 1; i >= 0; i--){
+            int j = input.length() - 1 - i;
+            reversed[j] = input.charAt(i);
+        }
+        String reverse = new String(reversed);
+        if(input.equalsIgnoreCase(reverse)){
+            isPalindrome = true;
         }
 
-        if (s.charAt(start) != s.charAt(end)) {
-            return false;
-        }
-        return check(s, start + 1, end - 1);
+        return isPalindrome;
     }
-}
+
+    }
+
+
+
+
