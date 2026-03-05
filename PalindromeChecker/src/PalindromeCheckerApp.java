88ad5371eb7a98616e9bd907457aae616eb1f829
ShreySharma15 <shrey1atharv@gmail.com>
2026-03-05 09:23:15 +0530

    UC10: Case-Insensitive & Space-Ignored Palindrome

--- PalindromeChecker/src/PalindromeCheckerApp.java
+++ PalindromeChecker/src/PalindromeCheckerApp.java
@@ -1,39 +1,35 @@
 /*
  * MAIN CLASS - PalindromeCheckerApp
- * UC9 : Recursive Palindrome Checker
+ * UC10: Case-Insensitive & Space-Ignored Palindrome
  *
  * Description:
  * This class represents the palindrome checker app
  *
  * At this stage, the application:
- * - Validates a palindrome using recursion
+ * - Validates a palindrome after normalisation
  * - comparing start and end characters
  * - Displays formatted result on console
  *
  *
  * @author Shrey Sharma
- * @version 9.0
+ * @version 9=10.0
  */
 
 
+import java.util.Locale;
+
 public class PalindromeCheckerApp {
     public static void main(String[] args) {
-        String input = "madam";
-
-        boolean result = check(input, 0, input.length() - 1);
-
-        System.out.println("Input : " + input);
-        System.out.println("Is Palindrome? : " + result);
-    }
-
-    private static boolean check(String s, int start, int end){
-        if (start >= end) {
-            return true;
+        String input = "A man a plan a canal Panama";
+        boolean isPalindrome = true;
+        String inputLowerCase= input.toLowerCase();
+        String normalised = inputLowerCase.replace(" ","");
+        for(int i=0; i<normalised.length()/2; i++){
+            if(normalised.charAt(i) != normalised.charAt(normalised.length() -1 -i)){
+                isPalindrome = false;
+            }
         }
-
-        if (s.charAt(start) != s.charAt(end)) {
-            return false;
-        }
-        return check(s, start + 1, end - 1);
+        System.out.println("Input : " + normalised);
+        System.out.println("Is Palindrome? : " + isPalindrome);
     }
 }
